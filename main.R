db62cf4584180acbb7b813587e8a72eb675c9bd8
rubengura <ruben.guerrero.ramirez@gmail.com>
2019-09-12 23:12:09 +0200

    correlations, graphical representation and PCA with 4 components added to the analysis.

--- main.R
+++ main.R
@@ -8,3 +8,10 @@ library(ggcorrplot)
 humor_df <- read_csv2('humor_dataset.csv')
 
 head(humor_df, 20)
+summary(humor_df)
+
+correlations <- cor(humor_df[, 1:32])
+ggcorrplot(correlations, hc.order = TRUE, type = "lower")
+
+
+PCA(humor_df[, 1:32], ncp = 4, graph = T)
