434642897c8af11fd8041bec12d1226c7759d2c4
rubengura <ruben.guerrero.ramirez@gmail.com>
2019-09-13 15:53:47 +0200

    Added comments on the code.

--- main.R
+++ main.R
@@ -1,17 +1,21 @@
 # Introduction to PCA.
 
+# Necessary libraries
 library(tidyverse)
 library(FactoMineR)
 library(factoextra)
 library(ggcorrplot)
 
+# Reading dataset
 humor_df <- read_csv2('humor_dataset.csv')
 
+# Getting to know a little bit the data
 head(humor_df, 20)
 summary(humor_df)
 
+# Visualization of correlations between questions
 correlations <- cor(humor_df[, 1:32])
 ggcorrplot(correlations, hc.order = TRUE, type = "lower")
 
-
+# Principal Component Analysis
 PCA(humor_df[, 1:32], ncp = 4, graph = T)
